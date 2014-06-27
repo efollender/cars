53dbfc49ef66ff3893f7b4a48c6082ac2f2ffc00
Emma Follender <efollender@gmail.com>
2014-06-27 14:52:06 -0400

    Added methods total_car_count and cars_per_color

--- cars.rb
+++ cars.rb
@@ -1,13 +1,29 @@
 class Car
+
+	@@total_car_count = 0
+	@@cars_per_color = {}
+
 	def to_s()
 		"I'm a car! I've driven #{@distance} and have #{@fuel} gallons gas left"
 	end
 
-	def initialize()
+	def initialize(color)
 		@fuel = 10
 		@distance = 0
+		@color = color
+		@@total_car_count +=1
+		if @@cars_per_color[@color]
+			@@cars_per_color[@color] += 1
+		else
+			@@cars_per_color[@color] = 1
+		end
 	end
-
+	def self.cars_per_color
+		@@cars_per_color
+	end
+	def self.total_car_count
+		@@total_car_count
+	end
 	def drive(miles)
 		if (@fuel - miles/20.0) >= 0
 			@distance += miles
@@ -26,14 +42,14 @@ class Car
 	end
 end
 
-car_a = Car.new()
-car_b = Car.new()
-puts car_a
-puts car_b
-car_a.drive(10)
-puts car_a
-puts car_b
-car_a.drive(232)
-car_b.drive(117)
-puts car_a
-puts car_b
+# car_a = Car.new()
+# car_b = Car.new()
+# puts car_a
+# puts car_b
+# car_a.drive(10)
+# puts car_a
+# puts car_b
+# car_a.drive(232)
+# car_b.drive(117)
+# puts car_a
+# puts car_b
